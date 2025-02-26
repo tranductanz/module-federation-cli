b6049dcc4b0c796d6ab1f9a8c90f2cb0a60e7634
Trần Đức Tân <tantran1610@gmail.com>
2025-02-26 14:51:22 +0700

    config...

--- templates/modulewithRedux/buildBundle.sh
+++
@@ -1,1 +0,0 @@
-yarn react-native webpack-bundle --platform ios --webpackConfig webpack-production.config.mjs --entry-file index.js  --dev=false && cd build && tar -cvf PackageBundle.tar PackageBundle
